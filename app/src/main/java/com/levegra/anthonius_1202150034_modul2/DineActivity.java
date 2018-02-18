c6695f1f5714d3b36363f731a2e13f76bb1a9bea
Anthonius Ongkowijoyo <anthonius@komers.id>
2018-02-18 23:38:59 +0700

    Update many pages

---
+++ app/src/main/java/com/levegra/anthonius_1202150034_modul2/DineActivity.java
@@ -0,0 +1,13 @@
+package com.levegra.anthonius_1202150034_modul2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dine);
+    }
+}
---
+++ app/src/main/java/com/levegra/anthonius_1202150034_modul2/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.levegra.anthonius_1202150034_modul2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
